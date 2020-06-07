09d2e89ed981ec08540a91cb7e96ac2220df73aa
jamie thain <jajt722@uowmail.edu.au>
2020-06-07 13:42:52 +1000

    small issue

--- Web.java
+++ Web.java
@@ -16,7 +16,7 @@ public class Web {
         double amount = Util.random_money(10000); //partition testing?
         
         //exchange(String.format("https://www.xe.com/currencyconverter/convert/?Amount=%s&From=%s&To=%s", "60.06", "AUD", "GBP"), "converterresult-toAmount");
-        exchange(String.format("https://www.x-rates.com/calculator/?amount=%s&from=%s&to=%s", "60.06", "AUD", "GBP"), "ccOutputRslt\">");
+        exchange(String.format("https://www.x-rates.com/calculator/?amount=%s&from=%s&to=%s", "60.06", "AUD", "GBP"), "<div class=\"ccOutputBx\">");
         exchange(String.format("https://www.calculator.net/currency-calculator.html?eamount=%s&efrom=%s&eto=%s&type=1&x=0&y=0", "60.06", "AUD", "GBP"), "<font color=green><b>");
     }
 
